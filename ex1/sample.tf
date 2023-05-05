927907b2fbc7d9a721d233bcbde685232776290a
noolivaralakshmi <noolivaralakshmi@gmail.com>
2023-05-05 13:27:18 -0400

    terraform sample change

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -4,11 +4,15 @@ data "aws_ami" "centos" {
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
+data "aws_security_group" "selected" {
+  name = allow-all
+}
 
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
+
 
   tags = {
     Name = "HelloWorld"
@@ -17,5 +21,9 @@ resource "aws_instance" "web" {
 
 output "ami"{
   value =  data.aws_ami.centos.image_id
+}
+
+output "security"{
+  value = data.aws_security_group.selected.id
 }
 
